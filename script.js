4ddadfb044b13fb3c5a90ef142cfba37dd2255d9
ali93abdullahi <ali93alexander@gmail.com>
2025-04-30 15:09:22 -0400

    Changes

---
+++ script.js
@@ -0,0 +1,25 @@
+<script>
+  tsParticles.load("tsparticles", {
+    fullScreen: { enable: true },
+    background: { color: { value: "#00000000" } }, // transparent
+    particles: {
+      number: { value: 120 },
+      color: { value: "#ffffff" },
+      shape: { type: "circle" },
+      opacity: {
+        value: 0.5,
+        random: true
+      },
+      size: {
+        value: 1.5,
+        random: true
+      },
+      move: {
+        enable: true,
+        speed: 0.5,
+        direction: "none",
+        outModes: { default: "out" }
+      }
+    }
+  });
+</script>
